072fb949f7cf0d8f7d4a4562149ef991ec5a3a6e
Jason <jason.stickel@live.com>
2018-04-19 19:54:57 -0700

    finished

---
+++ src/containers/Counter/store/actions.js
@@ -0,0 +1,1 @@
+// alternative folder structure
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 // reducers are the place where you are meant to update state (pure, sync code only) - it's a core redux concept, action creators are not (used for async code)
 
@@ -6,23 +7,18 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+};
+
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
             // Change data here
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
     }
     return state;
 };
